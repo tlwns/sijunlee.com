057cfafec3bad1ccf7f7a6e714cd411e4449401b
Sijun Lee <sijun.lee03@gmail.com>
2025-02-14 21:54:17 +1100

    feat: stubbed mainlayout and sidebar

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react';
+
+const Sidebar = () => {
+  return (
+    <Flex dir='column'>
+      <Flex>{/* Headers */}</Flex>
+      <Flex>{/* About me block */}</Flex>
+    </Flex>
+  );
+};
+
+export default Sidebar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,13 @@
+import { Flex } from '@chakra-ui/react';
+import Sidebar from '../components/ui/Sidebar';
+
 const MainLayout = () => {
-  return <></>;
+  return (
+    <Flex flex={1} bgColor={'gray.100'} flexDir={'column'}>
+      <Sidebar />
+      <Flex></Flex>
+    </Flex>
+  );
 };
 
 export default MainLayout;
