326d85ffb88a2c26590e8717a6303c7844fd4bd6
Sijun Lee <sijun.lee03@gmail.com>
2025-02-15 15:58:51 +1100

    chore: added eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-export default tseslint.config(
-***REMOVED*** ignores: ['dist'] },
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+***REMOVED***files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+***REMOVED***languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ***REMOVED***
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-  ***REMOVED***
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-  ***REMOVED***
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-      ***REMOVED*** allowConstantExport: true },
-      ],
-  ***REMOVED***
-***REMOVED***
-)
