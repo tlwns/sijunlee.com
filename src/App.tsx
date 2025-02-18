13836c450bc4fa42b211609713c1a40b03c7986f
Sijun Lee <sijun.lee03@gmail.com>
2025-02-18 22:35:56 +1100

    feat: stubbed aboutpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import RootLayout from './layouts/RootLayout';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<RootLayout />}>
           <Route index element={<HomePage />} />
+          <Route path='about' element={<AboutPage />} />
         </Route>
         <Route path='*' element={<div>404 Not Found</div>} />
       </Routes>
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react';
+import MainLayout from '../layouts/MainLayout';
+
+const AboutPage = () => {
+  return (
+    <MainLayout>
+      <Flex>About Page</Flex>
+    </MainLayout>
+  );
+};
+
+export default AboutPage;
