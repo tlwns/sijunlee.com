941fcfd5442f3b006bb7622587e5de70a1734046
Sijun Lee <sijun.lee03@gmail.com>
2025-02-26 19:14:00 +1100

    fix: remove about me route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import RootLayout from './layouts/RootLayout';
 import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
 import ResumePage from './pages/ResumePage';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <Routes>
         <Route path='/' element={<RootLayout />}>
           <Route index element={<HomePage />} />
-          <Route path='about' element={<AboutPage />} />
           <Route path='resume' element={<ResumePage />} />
         </Route>
         <Route path='*' element={<div>404 Not Found</div>} />
