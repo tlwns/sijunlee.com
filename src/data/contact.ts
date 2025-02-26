b0f80eac5c7c8572ad9293120d8674479623d666
Sijun Lee <sijun.lee03@gmail.com>
2025-02-26 17:08:02 +1100

    fix:

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa6';
+***REMOVED***
 
 ***REMOVED***
 ***REMOVED***
@@ -10,11 +10,6 @@ import { FaGithub, FaLinkedinIn, FaEnvelope } from 'react-icons/fa6';
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-    label: 'Email',
-    link: 'mailto:sijun.lee03@gmail.com',
-    icon: FaEnvelope,
 ***REMOVED***
 ***REMOVED***
 
