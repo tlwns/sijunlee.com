1c82d4dde75c6d5a02b8a7315cef57b2a1ada799
Sijun Lee <sijun.lee03@gmail.com>
2025-11-04 14:19:55 +1100

    refactor: improve layout and styling in ResumePage and MainHeader components

--- components/ui/MainHeader.tsx
+++ components/ui/MainHeader.tsx
@@ -8,17 +8,16 @@ interface MainHeaderProps {
 
 const MainHeader = ({ title, description, children }: MainHeaderProps) => {
   return (
-    <Flex
-      flexDir='column'
-      gap={6}
-      borderBottomWidth={1.1}
-      borderColor={'border'}
-      pb={10}
-    >
-      <Heading fontWeight={'bold'} as={'h1'} fontSize={'8xl'}>
+    <Flex flexDir='column' gap={6} pb={24}>
+      <Heading
+        fontWeight={'bold'}
+        as={'h1'}
+        fontSize={{ base: '4xl', md: '5xl', lg: '7xl' }}
+        letterSpacing={'tighter'}
+        textAlign={'center'}
+      >
         {title}
       </Heading>
-
       {description && (
         <Text fontWeight={'light'} fontSize={'sm'} letterSpacing={'wider'}>
           {description}
