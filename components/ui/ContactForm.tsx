e2b84c671c2257756805a21b418809e40e6ddb35
Sijun Lee <sijun.lee03@gmail.com>
2025-11-08 13:43:55 +1100

    feat: enhance ContactForm with error handling and input state management

--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -6,16 +6,14 @@ import { State, submitContactForm } from '@/lib/actions/submitContactForm';
 import { useActionState } from 'react';
 
 const ContactForm = () => {
-  const initialState: State = {
-    errors: {},
-  };
+  const initialState: State = {};
 
   const [state, formAction] = useActionState(submitContactForm, initialState);
 
   return (
     <form action={formAction} style={{ width: '80%' }}>
       <Fieldset.Root gap={4}>
-        <Field.Root orientation='horizontal'>
+        <Field.Root invalid={state.errors?.name !== undefined}>
           <Field.Label hidden>Your Name</Field.Label>
           <Input
             type='text'
@@ -23,9 +21,14 @@ const ContactForm = () => {
             size='lg'
             placeholder='Your Name'
             focusRingColor={'blue.fg'}
+            name='name'
+            defaultValue={state.inputs?.name}
           />
+          {state.errors?.name && (
+            <Field.ErrorText>{state.errors?.name.join(', ')}</Field.ErrorText>
+          )}
         </Field.Root>
-        <Field.Root orientation='horizontal'>
+        <Field.Root invalid={state.errors?.email !== undefined}>
           <Field.Label hidden>Your Email</Field.Label>
           <Input
             type='email'
@@ -33,9 +36,14 @@ const ContactForm = () => {
             size='lg'
             placeholder='Your Email'
             focusRingColor={'blue.fg'}
+            name='email'
+            defaultValue={state.inputs?.email}
           />
+          {state.errors?.email && (
+            <Field.ErrorText>{state.errors?.email.join(', ')}</Field.ErrorText>
+          )}
         </Field.Root>
-        <Field.Root orientation='horizontal'>
+        <Field.Root invalid={state.errors?.subject !== undefined}>
           <Field.Label hidden>Subject</Field.Label>
           <Input
             type='text'
@@ -43,9 +51,16 @@ const ContactForm = () => {
             size='lg'
             placeholder='Subject'
             focusRingColor={'blue.fg'}
+            name='subject'
+            defaultValue={state.inputs?.subject}
           />
+          {state.errors?.subject && (
+            <Field.ErrorText>
+              {state.errors?.subject.join(', ')}
+            </Field.ErrorText>
+          )}
         </Field.Root>
-        <Field.Root orientation='horizontal'>
+        <Field.Root invalid={state.errors?.message !== undefined}>
           <Field.Label hidden>Message</Field.Label>
           <Textarea
             focusRingColor={'blue.fg'}
@@ -53,7 +68,14 @@ const ContactForm = () => {
             size='lg'
             placeholder='Message'
             autoresize
+            name='message'
+            defaultValue={state.inputs?.message}
           />
+          {state.errors?.message && (
+            <Field.ErrorText>
+              {state.errors?.message.join(', ')}
+            </Field.ErrorText>
+          )}
         </Field.Root>
 
         <Button
--- lib/actions/submitContactForm.ts
+++ lib/actions/submitContactForm.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { ContactFormSchema } from '@/schemas/contactFormSchema';
+import { Resend } from 'resend';
 
 export type State = {
   errors?: {
@@ -9,32 +10,53 @@ export type State = {
     subject?: string[];
     message?: string[];
   };
+  inputs?: {
+    name?: string;
+    email?: string;
+    subject?: string;
+    message?: string;
+  };
 };
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function submitContactForm(prevState: State, formData: FormData) {
-  const validatedFields = ContactFormSchema.safeParse({
-    name: formData.get('name'),
-    email: formData.get('email'),
-    subject: formData.get('subject'),
-    message: formData.get('message'),
-  });
+  const rawData = {
+    name: formData.get('name') as string,
+    email: formData.get('email') as string,
+    subject: formData.get('subject') as string,
+    message: formData.get('message') as string,
+  };
+
+  const validatedFields = ContactFormSchema.safeParse(rawData);
 
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
+      inputs: rawData,
     };
   }
 
   const { name, email, subject, message } = validatedFields.data;
+
   try {
-    // Simulate sending an email or storing the message
-    console.log('Contact Form Submitted:', {
-      name,
-      email,
-      subject,
-      message,
+    const { data, error } = await resend.emails.send({
+      from: 'GymTrack Support Contact <onboarding@resend.dev>',
+      to: [`${process.env.CONTACT_EMAIL}`],
+      subject: `Contact Form Submission: ${subject}`,
+      html: `
+        <h1>New Contact Form Submission</h1>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Subject:</strong> ${subject}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `,
     });
 
+    if (error) {
+      throw new Error(error.message);
+    }
     return {};
   } catch (error) {
     throw new Error((error as Error).message);
